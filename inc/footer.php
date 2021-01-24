1a6a415e391f12893c083ce571b744c0ff792506
Shafqat Hasan <60235150+shafhasan@users.noreply.github.com>
2021-01-25 00:18:33 +0600

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+</section>
+<section class="footeroption">
+<p>© <?php echo date("Y"); ?> powered by সাধারণ ছাত্র </p>
+ <h2><?php echo "vehiclebookingservice.com"; ?></h2>
+</section>
+</div>
+</body>
+</html>
