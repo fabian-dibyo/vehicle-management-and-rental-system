fce20de58642b9eb5df54960c56f7cf94248d174
Dibyo Fabian Dofadar <60013050+fabian-dibyo@users.noreply.github.com>
2021-04-17 14:14:37 +0600

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,7 @@
 </section>
 <section class="footeroption">
-<p>© <?php echo date("Y"); ?> powered by সাধারণ ছাত্র </p>
- <h2><?php echo "vehiclebookingservice.com"; ?></h2>
+<p>© <?php echo date("Y"); ?> powered by Show Stoppers!!! </p>
+ <h2><?php echo "vmrs-cse370.herokuapp.com"; ?></h2>
 </section>
 </div>
 </body>
